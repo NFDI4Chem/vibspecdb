3802916bfdf0f72d997423f8a26eeb33f6e32ada
Darina laptop <darina.storozhuk@uni-jena.de>
2021-10-29 18:45:20 +0200

    minio docker compose

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class UploadController extends Controller
+{
+
+  public function store(Request $request)
+  {
+    Storage::disk('minio')->put('example.txt', 'Contents');
+    // Storage::disk('minio')->put('hello.json', '{"hello": "world"}', 'public');
+    return [];
+  }
+
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -52,7 +52,18 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
-
+        
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'url' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            // 'bucket_endpoint' => false,
+        ],
     ],
 
     /*
