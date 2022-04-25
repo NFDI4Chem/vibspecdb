1785522772207b9fc5f5fb222009231063549d7b
Nazar Stefaniuk <nazar.stefaniuk@uni-jena.de>
2022-04-25 13:31:00 +0200

    Fixes to have Table shared integrated
    - fixes to tailwind config
    - updated Table shared component
    - fixed version of vue-components

--- resources/js/Shared/Table/Table.vue
+++ resources/js/Shared/Table/Table.vue
@@ -1,65 +1,115 @@
 <template>
-  <UniTable
-      :data="data"
-      :header="header"
-      :visible="visible"
-      :actions="actions"
-      :title="title"
-  />
+    <UniTable
+        :data="data"
+        :header="header"
+        :visible="visible"
+        :actions="actions"
+        :title="title"
+        :style="style"
+        class="m-3"
+    />
 </template>
 
 <script setup>
 import { UniTable } from "@/packages/serviceappvuecomponents/src";
 
+import {
+  ChevronDownIcon,
+  TrashIcon,
+  PlusCircleIcon,
+  PencilAltIcon,
+} from "@heroicons/vue/outline";
+
 const title = "Base Table Template";
 
-const header = ["Name", "Title", "Email", "Role"];
+const header = {
+    name: "Name",
+    title: "Title",
+    email: "Email",
+    role: "Role",
+};
+
+const style = {
+    greyRows: true,
+    divideX: true,
+    themeColor: "cyan",
+    rowActions: "icons",
+};
+
+const GlobalRowAction = (key) => {
+  console.log(`Action "${key}" here`);
+  switch (key) {
+    case "add":
+      alert(`Action type: ${key}`);
+      break;
+    case "delete":
+      alert(`Rows to be deleted (ids): ` + JSON.stringify(checkedRows.value));
+      break;
+    default:
+      alert("Global RowAction");
+  }
+};
+
+const actions = [
+  {
+    key: "add",
+    icon: PlusCircleIcon,
+    action: GlobalRowAction,
+    visible: true,
+  },
+  {
+    key: "delete",
+    icon: TrashIcon,
+    action: GlobalRowAction,
+    visible: true,
+  },
+];
 
 const data = {
     rows: [
-        [
-            "Lindsay Walton",
-            "Front-end Developer",
-            "lindsay.walton@example.com",
-            "Member",
-        ],
-        [
-            "Breddi Pitt",
-            "Backs-end Developer",
-            "bredi.pitti@example.com",
-            "Admin",
-        ],
+        {
+            name: "Lindsay Walton",
+            title: "Front-end Developer",
+            email: "lindsay.walton@example.com",
+            role: "Member",
+        },
+        {
+            name: "Breddi Pitt",
+            title: "Backs-end Developer",
+            email: "bredi.pitti@example.com",
+            role: "Admin",
+        },
     ],
     actions: [
         {
+            key: "details",
+            title: "Details",
+            icon: ChevronDownIcon,
+            visible: true,
+            action: (rowId, actionKey) => DetailsAction(rowId, actionKey),
+        },
+        {
+            key: "edit",
             title: "Edit",
-            action: () => {
-                console.log("Edit action");
+            icon: PencilAltIcon,
+            visible: true,
+            action: (rowId, actionKey) => {
+                alert(`Edit action rowId: ${rowId}, actionKey: ${actionKey}`);
             },
         },
         {
+            key: "delete",
             title: "Delete",
-            action: () => {
-                console.log("Delete action");
+            icon: TrashIcon,
+            visible: true,
+            action: (rowId, actionKey) => {
+                alert(`Delete action rowId: ${rowId}, actionKey: ${actionKey}`);
             },
         },
     ],
 };
 
-const actions = [
-    {
-        name: "Create",
-        action: () => {
-            console.log("Action 1 here");
-        },
-    },
-    {
-        name: "Delete",
-        action: () => {
-            console.log("Action 2 here");
-        },
-    },
-];
+
 </script>
 
 <style lang="scss" scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,43 @@ module.exports = {
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/js/**/*.vue",
-        './vendor/spatie/laravel-support-bubble/config/**/*.php',
-        './vendor/spatie/laravel-support-bubble/resources/views/**/*.blade.php',
+        "./vendor/spatie/laravel-support-bubble/config/**/*.php",
+        "./vendor/spatie/laravel-support-bubble/resources/views/**/*.blade.php",
     ],
-
+    safelist: [
+        {
+            pattern: /grid-cols-/,
+            variants: ["md", "lg", "hover", "focus"],
+        },
+        {
+            pattern: /box-/,
+            variants: ["lg", "hover", "focus"],
+        },
+        {
+            pattern: /h-/,
+            variants: ["lg", "hover", "focus"],
+        },
+        {
+            pattern: /w-/,
+            variants: ["lg", "hover", "focus"],
+        },
+        {
+            pattern: /border-/,
+            variants: ["lg", "hover", "focus"],
+        },
+        {
+            pattern: /bg-/,
+            variants: ["lg", "hover", "focus"],
+        },
+        {
+            pattern: /text-/,
+            variants: ["lg", "hover", "focus"],
+        },
+        {
+            pattern: /^ring-/,
+            variants: ["lg", "hover", "focus"],
+        },
+    ],
     theme: {
         extend: {
             fontFamily: {
@@ -21,16 +54,16 @@ module.exports = {
             colors: {
                 rose: colors.rose,
                 sky: colors.sky,
-                'teal': {
-                    100: '#019DBB',
-                    200: '#019DBB',
-                    300: '#019DBB',
-                    400: '#02abc9',
-                    500: '#019DBB',
-                    600: '#0088a0',
-                    700: '#019DBB',
-                    800: '#019DBB',
-                    900: '#019DBB',
+                teal: {
+                    100: "#019DBB",
+                    200: "#019DBB",
+                    300: "#019DBB",
+                    400: "#02abc9",
+                    500: "#019DBB",
+                    600: "#0088a0",
+                    700: "#019DBB",
+                    800: "#019DBB",
+                    900: "#019DBB",
                 },
             },
         },
