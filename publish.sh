145b38e701d15fcb5afb39dfd508e291207e1a78
Nazar Stefaniuk <nazar.stefaniuk@uni-jena.de>
2022-07-02 18:07:30 +0200

    updated gilabci, added publish script

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DOCKER_USERNAME=docker.photonicdata.science
+
+FPM_NAME=serviceapp_laravel_fpm
+FPM_TAG=v1.0.28
+
+NGINX_NAME=serviceapp_nginx
+NGINX_TAG=v0.1.0
+
+docker-compose build
+
+docker image tag serviceapp_php:latest ${DOCKER_USERNAME}/${FPM_NAME}:${FPM_TAG}
+docker image tag serviceapp_nginx:latest ${DOCKER_USERNAME}/${NGINX_NAME}:${NGINX_TAG}
+
+docker push ${DOCKER_USERNAME}/${FPM_NAME}:${FPM_TAG}
+docker push ${DOCKER_USERNAME}/${NGINX_NAME}:${NGINX_TAG}
