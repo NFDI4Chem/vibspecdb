5d9929ce1fafec1cf79fddf4e06be70290d5e5f5
Nazar Stefaniuk <nazar.stefaniuk@uni-jena.de>
2022-07-04 22:21:14 +0200

    horizon added , composer major updates, docker compose and dockerfile updated

---
+++ resources/views/email/demo.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How To Send E-mail Using Queue In Laravel 9 - Websolutionstuff</title>
+</head>
+<body>
+   
+<center>
+<h2>
+	<a href="https://google.com">Visit Our Website : Google!</a>
+</h2>
+</center>
+  
+<p>Hello,</p>
+
+<p>This is a test mail. This mail send using queue listen in laravel 9.</p>  
+
+<strong>Thanks & Regards.</strong>
+
+</body>
+</html>
