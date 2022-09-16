7db0b6f6905740e0ff73dcbfdb7cf6045b7643ff
Nazar Stefaniuk <nazar.stefaniuk@uni-jena.de>
2022-09-16 15:42:43 +0200

    move event listener to persistent layout

--- resources/js/Layouts/PersistentLayout.vue
+++ resources/js/Layouts/PersistentLayout.vue
@@ -6,8 +6,24 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
+import FilesModal from '@/Components/UploadModal/FilesModal.vue';
+import { useForm, usePage } from "@inertiajs/inertia-vue3";
 
-import FilesModal from '@/Components/UploadModal/FilesModal.vue'
+const user = computed(() => usePage().props.value.user);
+
+let  channel = Echo.channel('user-channel');
+channel
+    .listen('.UserEvent',function (data){
+        console.log(data);
+        console.log('listended');
+    })
+
+Echo.private(`App.Models.User.${user.value.id}`)
+    .listen('.server.created', (e) => {
+        console.log('private channel works');
+});
+
 
 const title = 'Upload Files';
 
