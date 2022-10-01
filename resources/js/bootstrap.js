b8bbec64dba52a87904e3a6cdd4b7b786beb0f53
Nazar Stefaniuk <nazar.stefaniuk@uni-jena.de>
2022-10-01 22:44:03 +0200

    fix pusher host

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,7 +28,7 @@ window.Pusher = Pusher;
 window.Echo  = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: '127.0.0.1', // import.meta.env.VITE_PUSHER_HOST,
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_PUSHER_PORT,
     wssPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: false,
