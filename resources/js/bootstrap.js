aeda1b16be23910e0a9839c617bde136b2bc1315
Nazar Stefaniuk <nazar.stefaniuk@uni-jena.de>
2022-11-03 19:38:58 +0100

    [typo] [fix] Webhook word

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,7 +30,6 @@ window.Pusher = Pusher;
 window.Echo  = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    // host: import.meta.env.VITE_PUSHER_HOST,
     wsHost: import.meta.env.VITE_PUSHER_HOST,
     // wsPort: import.meta.env.VITE_PUSHER_PORT,
     // wssPort: import.meta.env.VITE_PUSHER_PORT,
