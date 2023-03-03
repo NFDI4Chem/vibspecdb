efeaca3547acf4e933e9890b3ab277f9c11ffed9
Darina Strorozhuk <darina.storozhuk@uni-jena.de>
2023-03-03 19:51:58 +0100

    docker compose added raman-api-service
    - updated laravel config example file
    - update services config

--- config/services.php
+++ config/services.php
@@ -13,6 +13,9 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+    'fastapi-ramanmetrix' => [
+        'url' => env('RAMAN_FASTAPI_SERVICE_URL', NULL)
+    ],
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
