7647b80902a0070200362d500e4e51aa21992054
Darina Strorozhuk <darina.storozhuk@uni-jena.de>
2023-03-22 11:41:31 +0100

    added new DB migration

---
+++ database/migrations/2022_10_06_130848_update_projects_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('schema_version', 2048)->nullable();
+            $table->string('internal_status', 2048)->nullable();
+            $table->timestamp('deleted_on')->nullable();
+        });
+
+        Schema::table('studies', function (Blueprint $table) {
+            $table->string('internal_status', 2048)->nullable();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('schema_version');
+            $table->dropColumn('internal_status');
+        });
+
+        Schema::table('studies', function (Blueprint $table) {
+            $table->dropColumn('internal_status');
+        });
+
+    }
+};
