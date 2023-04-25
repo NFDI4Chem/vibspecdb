d773342fda252bde7e8acb5e2c62f4de59f55b70
Darina Strorozhuk <darina.storozhuk@uni-jena.de>
2023-04-25 13:31:39 +0200

    added fix on SplitterToggle button visibility with composition api

--- resources/js/Pages/Study/Index.vue
+++ resources/js/Pages/Study/Index.vue
@@ -4,11 +4,14 @@
       <template #scontent>
         <div class="bg-white shadow-md flex flex-col flex-1 mb-0">
           <div class="px4 py-4 project-tabs w-full">
-            <SplitterToggler v-model:split_views="split_views" />
+            <SplitterToggler
+              v-if="StudyTab === 0"
+              v-model:split_views="split_views"
+            />
             <w-tabs
               :items="tabs"
               class="w-full rounded-none border-0"
-              v-model="tab"
+              v-model="StudyTab"
             >
               <template #item-content="{ index }">
                 <UploadFiles
@@ -39,28 +42,31 @@ import Metadata from '@/Pages/Study/Partials/Tabs/Metadata.vue'
 import UploadFiles from '@/Pages/Study/Partials/Tabs/UploadFiles.vue'
 
 import { split_views } from '@/VueComposable/useStudyLayer'
+import { StudyTab } from '@/VueComposable/useStudy'
 
 const props = defineProps(['study', 'project', 'files', 'tab', 'projects'])
 
-const tab = computed(() => {
+const tabs = [
+  { title: 'Workspace', content: 'Files Tree and uploader.' },
+  { title: 'Common Info', content: 'Study Common Info.' },
+  { title: 'Study Metadata', content: 'Custom Metadata.' },
+  // { title: 'Files', content: 'Files Tree and preview.' },
+]
+
+const onMountedTab = () => {
   let t = props?.tab
   if (t < 1 || t > tabs.length) t = 1
   return t - 1
-})
+}
 
-const tabs = [
-  { title: 'Upload', content: 'Files Tree and uploader.' },
-  { title: 'Info', content: 'Study Common Info.' },
-  { title: 'Metadata', content: 'Custom Metadata.' },
-  // { title: 'Files', content: 'Files Tree and preview.' },
-]
+StudyTab.value = onMountedTab()
 </script>
 
 <style lang="scss">
 .project-tabs {
   .w-tabs__bar-item {
     margin-right: 10px;
-    width: 80px;
+    width: 140px;
     height: 35px;
   }
   .w-tabs__content {
---
+++ resources/js/VueComposable/useStudy.js
@@ -0,0 +1,2 @@
+import { ref } from 'vue'
+export const StudyTab = ref(0)
